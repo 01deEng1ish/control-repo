a4f77faf37336a76183149cd2ab38ce92c3bc00d
Kristopher English <kristopher.english@gmail.com>
2019-09-09 18:37:54 -0400

    Switch GitLab to Enterprise Edition

--- site-modules/profile/manifests/gitlab.pp
+++ site-modules/profile/manifests/gitlab.pp
@@ -2,7 +2,8 @@ class profile::gitlab {
   include profile::firewall::allow_http
 
   class { 'gitlab':
-    external_url   => 'http://gitlab.oldenglish.net',
+    external_url            => 'http://gitlab.oldenglish.net',
+    manage_upstream_edition => 'ee',
   }
 
 }
